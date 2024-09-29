cbd0aad90e7f0988f9001b4cc6c6d68b31281023
Dennis <dennis.sadkowski@gmail.com>
2024-09-29 21:18:20 +0200

    small bugfix

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -26,19 +26,12 @@ SimpleSamplerAudioProcessorEditor::~SimpleSamplerAudioProcessorEditor()
 //==============================================================================
 void SimpleSamplerAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    /*g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);*/
-    g.fillAll (juce::Colours::black);
+    g.fillAll (juce::Colours::darkgrey);
 }
 
 void SimpleSamplerAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
-
     mainComponents.setBounds (10, 10, getWidth() - 20, getHeight() - 20);
 }
