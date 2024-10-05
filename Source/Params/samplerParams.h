df87c1c99f4b25847443d6e1ea1eecad11e81bca
dennis <dennis.sadkowski@gmail.com>
2024-10-05 11:00:27 +0200

    changed double to float

--- Source/Params/samplerParams.h
+++ Source/Params/samplerParams.h
@@ -5,10 +5,10 @@
 class SamplerParams
 {
 public:
-    SamplerParams() {};
-    ~SamplerParams() {};
-    void setGain(float gainToUse) { gain = gainToUse; };
-    float getGain() { return std::pow(10, gain / 20); };
+    SamplerParams() {}
+    ~SamplerParams() {}
+    void setGain(float gainToUse) { gain = gainToUse; }
+    float getGain() { return std::pow(10, gain / 20); }
 
 private:
     float gain = { 0.0 };
