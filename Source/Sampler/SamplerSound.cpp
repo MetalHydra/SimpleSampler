67337750d58801e081ce7d6217445fa02c94147f
Dennis <dennis.sadkowski@gmail.com>
2024-10-09 00:04:24 +0200

    rename Sampler to SamplerSound

---
+++ Source/Sampler/SamplerSound.cpp
@@ -0,0 +1,186 @@
+#include "SamplerSound.h"
+
+using namespace nSamplerSound;
+nSamplerSound::SamplerSound::SamplerSound (const juce::String& soundName,
+                            juce::AudioFormatReader& source,
+                            const juce::BigInteger& notes,
+                            int midiNoteForNormalPitch,
+                            double attackTimeSecs,
+                            double releaseTimeSecs,
+                            double maxSampleLengthSeconds)
+    : name (soundName),
+      sourceSampleRate (source.sampleRate),
+      midiNotes (notes),
+      midiRootNote (midiNoteForNormalPitch)
+{
+    if (sourceSampleRate > 0 && source.lengthInSamples > 0)
+    {
+        length = jmin ((int) source.lengthInSamples,
+                       (int) (maxSampleLengthSeconds * sourceSampleRate));
+
+        data.reset (new AudioBuffer<float> (jmin (2, (int) source.numChannels), length + 4));
+
+        source.read (data.get(), 0, length + 4, 0, true, true);
+        params.attack  = static_cast<float> (attackTimeSecs);
+        params.release = static_cast<float> (releaseTimeSecs);
+    }
+}
+
+
+
+nSamplerSound::SamplerSound::~SamplerSound()
+{
+}
+
+bool nSamplerSound::SamplerSound::appliesToNote (int midiNoteNumber)
+{
+    return midiNotes[midiNoteNumber];
+}
+
+bool nSamplerSound::SamplerSound::appliesToChannel (int /*midiChannel*/)
+{
+    return true;
+}
+
+
+
+//==============================================================================
+nSamplerSound::SamplerVoice::SamplerVoice() {}
+nSamplerSound::SamplerVoice::~SamplerVoice() {}
+
+bool nSamplerSound::SamplerVoice::canPlaySound (juce::SynthesiserSound* sound)
+{
+    return dynamic_cast<const SamplerSound*> (sound) != nullptr;
+}
+
+void nSamplerSound::SamplerVoice::startNote (int midiNoteNumber, float velocity, juce::SynthesiserSound* s, int /*currentPitchWheelPosition*/)
+{
+    reverb.prepare (spec);
+    if (auto* sound = dynamic_cast<const SamplerSound*> (s))
+    {
+        pitchRatio = std::pow (2.0, (midiNoteNumber - sound->midiRootNote) / 12.0)
+                        * sound->sourceSampleRate / getSampleRate();
+
+        sourceSamplePosition = 0.0;
+        lgain = velocity;
+        rgain = velocity;
+
+
+
+        adsr.setSampleRate (sound->sourceSampleRate);
+        adsr.setParameters (sound->params);
+        adsr.noteOn();
+    }
+    else
+    {
+        jassertfalse; // this object can only play SamplerSounds!
+    }
+}
+
+void nSamplerSound::SamplerVoice::stopNote (float /*velocity*/, bool allowTailOff)
+{
+    if (allowTailOff)
+    {
+        adsr.noteOff();
+    }
+    else
+    {
+        clearCurrentNote();
+        adsr.reset();
+        reverb.reset();
+    }
+}
+
+void nSamplerSound::SamplerVoice::pitchWheelMoved (int /*newValue*/) {}
+void nSamplerSound::SamplerVoice::controllerMoved (int /*controllerNumber*/, int /*newValue*/) {}
+
+void nSamplerSound::SamplerVoice::applyGainToBuffer (juce::AudioBuffer<float>& buffer, float gain)
+{
+    buffer.applyGain (0, buffer.getNumSamples(), gain);
+    isReady = true;
+}
+
+//==============================================================================
+void nSamplerSound::SamplerVoice::renderNextBlock (juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples)
+{
+    //DBG("voicebuffer size: " + std::to_string(voiceBuffer.getNumSamples()));
+    //DBG("voicebuffer channels: " + std::to_string(voiceBuffer.getNumChannels()));
+    voiceBuffer.setSize(outputBuffer.getNumChannels(), numSamples);
+    if (auto* playingSound = static_cast<SamplerSound*> (getCurrentlyPlayingSound().get()))
+    {
+        //DBG("playing sound: " + playingSound->name);
+        auto& data = *playingSound->data;
+
+        const float* const inL = data.getReadPointer (0);
+        const float* const inR = data.getNumChannels() > 1 ? data.getReadPointer (1) : nullptr;
+
+        float* outL = outputBuffer.getWritePointer (0, startSample);
+        float* outR = outputBuffer.getNumChannels() > 1 ? outputBuffer.getWritePointer (1, startSample) : nullptr;
+        int cnt = 0;
+
+        for (int sample = 0; sample < numSamples; ++sample)   //(--numSamples >= 0)
+        {
+
+
+            auto pos = (int) sourceSamplePosition;
+            auto alpha = (float) (sourceSamplePosition - pos);
+            auto invAlpha = 1.0f - alpha;
+
+            // just using a very simple linear interpolation here..
+            float l = (inL[pos] * invAlpha + inL[pos + 1] * alpha);
+            float r = (inR != nullptr) ? (inR[pos] * invAlpha + inR[pos + 1] * alpha) : l;
+
+            auto envelopeValue = adsr.getNextSample();
+            lgain = playingSound->samplerParams.getGain();
+            rgain = lgain;
+            l *=  envelopeValue;
+            r *=  envelopeValue;
+            if (outR != nullptr)
+            {
+                voiceBuffer.setSample(0, sample, l);
+                voiceBuffer.setSample(1, sample, r);
+                //*outL++ += l;
+                //*outR++ += r;
+            }
+            else
+            {
+                voiceBuffer.setSample(0, sample, l + r);
+                //*outL++ += (l + r) * 2.5f;
+            }
+            sourceSamplePosition += pitchRatio;
+
+            if (sourceSamplePosition > playingSound->length)
+            {
+                stopNote (0.0f, false);
+                break;
+            }
+            cnt++;
+        }
+        //DBG("finished processed  samples " + std::to_string(cnt));
+
+        reverb.setParameters(playingSound->getReverbParameters());
+        auto audioBlock = juce::dsp::AudioBlock<float>(voiceBuffer);
+        auto context = juce::dsp::ProcessContextReplacing<float>(audioBlock);
+
+        reverb.process(context);
+
+
+
+        voiceBuffer.applyGain(0, cnt, lgain);
+        if (outR != nullptr)
+        {
+            for(int sample = 0; sample < cnt; ++sample)
+            {
+                *outL++ += voiceBuffer.getSample(0, sample);
+                *outR++ += voiceBuffer.getSample(1, sample);
+            }
+        }
+        else
+        {
+            for(int sample = 0; sample < cnt; ++sample)
+            {
+                *outL++ += voiceBuffer.getSample(0, sample);
+            }
+        }
+    }
+}
---
+++ Source/Sampler/SamplerSound.h
@@ -0,0 +1,106 @@
+#include <JuceHeader.h>
+#include "../Params/samplerParams.h"
+#include "juce_dsp/juce_dsp.h"
+
+namespace nSamplerSound {
+    class SamplerSound : public SynthesiserSound {
+    public:
+        SamplerSound(const juce::String &name,
+                     juce::AudioFormatReader &source,
+                     const juce::BigInteger &midiNotes,
+                     int midiNoteForNormalPitch,
+                     double attackTimeSecs,
+                     double releaseTimeSecs,
+                     double maxSampleLengthSeconds);
+
+
+        /** Destructor. */
+        ~SamplerSound() override;
+
+        //==============================================================================
+        [[nodiscard]]
+        const juce::String &getName() const
+
+        noexcept { return name; }
+
+        [[nodiscard]]
+        juce::AudioBuffer<float> *getAudioData() const
+
+        noexcept { return data.get(); }
+
+        //==============================================================================
+        void setEnvelopeParameters(juce::ADSR::Parameters parametersToUse) { params = parametersToUse; }
+
+        void setParameters(const SamplerParams &samplerParamsToUse) { samplerParams = samplerParamsToUse; }
+
+        void setReverbParameters(const juce::Reverb::Parameters &reverbParamsToUse) { reverbParams = reverbParamsToUse; }
+
+        juce::Reverb::Parameters getReverbParameters() { return reverbParams; }
+
+        //==============================================================================
+        bool appliesToNote(int midiNoteNumber) override;
+
+        bool appliesToChannel(int midiChannel) override;
+
+    private:
+        //==============================================================================
+        friend class SamplerVoice;
+
+        String name;
+        std::unique_ptr<juce::AudioBuffer<float>> data;
+        double sourceSampleRate;
+        BigInteger midiNotes;
+        int length = 0, midiRootNote = 0;
+
+        juce::ADSR::Parameters params;
+        SamplerParams samplerParams;
+        juce::dsp::Reverb::Parameters reverbParams;
+
+        JUCE_LEAK_DETECTOR (SamplerSound)
+    };
+
+    class SamplerVoice : public juce::SynthesiserVoice {
+    public:
+        //==============================================================================
+        /** Creates a SamplerVoice. */
+        SamplerVoice();
+
+
+        /** Destructor. */
+        ~SamplerVoice() override;
+
+        //==============================================================================
+        bool canPlaySound(juce::SynthesiserSound *) override;
+
+        void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound *, int pitchWheel) override;
+
+        void stopNote(float velocity, bool allowTailOff) override;
+
+        void pitchWheelMoved(int newValue) override;
+
+        void controllerMoved(int controllerNumber, int newValue) override;
+
+        void applyGainToBuffer (juce::AudioBuffer<float>& buffer, float gain);
+
+        void renderNextBlock(juce::AudioBuffer<float> &, int startSample, int numSamples) override;
+        //using juce::SynthesiserVoice::renderNextBlock;
+
+    private:
+        //==============================================================================
+        friend class SamplerSound;
+        double pitchRatio = 0;
+        double sourceSamplePosition = 0;
+        float lgain = 0.0, rgain = 0.0;
+        juce::ADSR adsr;
+        juce::dsp::ProcessSpec spec = {
+            .sampleRate = 44100.0f,
+            .maximumBlockSize = 512,
+            .numChannels = 2
+        };
+        juce::dsp::Reverb reverb;
+
+        std::atomic<bool> isReady { false };
+        juce::AudioBuffer<float> voiceBuffer; //{ 2, (44100 * 4) + 4 };
+        JUCE_LEAK_DETECTOR (SamplerVoice)
+    };
+}
