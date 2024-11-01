3f54393a81c2d14cf0b8cf9be0d670182fb07a1f
dennis <dennis.sadkowski@gmail.com>
2024-11-01 21:59:07 +0100

    Adding Biquad Structure

--- Source/Filters/Filters.h
+++ Source/Filters/Filters.h
@@ -1,5 +1,46 @@
 #include <JuceHeader.h>
 
+enum FilterType
+{
+    Lowpass,
+    Highpass,
+    Bandpass
+};
+
+struct FilterParameters
+{
+    float fc = 100.0f;
+    float Q = 0.707f;
+    float cutoff = 0.0f;
+};
+
+struct filterCoefficients
+{
+    float a0 = 0.0f;
+    float a1 = 0.0f;
+    float a2 = 0.0f;
+    float b1 = 0.0f;
+    float b2 = 0.0f;
+    float c0 = 1.0f;
+    float d0 = 0.0f;
+};
+
+class BiQaudFilter
+{
+public:
+    BiQaudFilter();
+    ~BiQaudFilter();
+    void setCutOffFrequency(float cutoff);
+    void setSamplingRate(float samplerate);
+    void updateParamters(FilterParameters& parameters);
+    filterCoefficients& getFilterParamters();
+    void processBlock(juce::AudioBuffer<float>& buffer);
+    void calculateCoefficients(FilterType filter);
+
+private:
+    filterCoefficients coefficients;
+};
+
 class LowpassFilter
 {
 public:
